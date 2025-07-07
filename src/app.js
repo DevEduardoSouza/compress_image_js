03a1ae663ca6635d5df2c3ad5135701587334d75
DevEduardoSouza <eduardosouza.din2@gmail.com>
2025-07-07 17:29:19 -0300

    feat: init

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("🚀 Projeto compress_image_js iniciado!");
+import sharp from "sharp";
+
+const inputPath = "teste.png";
+
+const compress_image = (inputPath) => {
+  try {
+    if (!inputPath) throw new Error("image input path not set");
+
+    const outputPath = `output_${Date.now()}.jpg`;
+
+    sharp("./src/input.png").jpeg({ quality: 50 }).toFile(outputPath);
+
+    console.log("Imagem comprimida salva em:", outputPath);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+compress_image(inputPath);
